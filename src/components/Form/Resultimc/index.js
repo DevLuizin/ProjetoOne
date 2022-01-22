35563b6d18c718e12a173e699b2e0f14fcd9af22
Luiz Carlos <luizcaetite@gmail.com>
2022-01-22 12:17:08 -0300

    Implementação-da-Lógica

--- src/components/Form/Resultimc/index.js
+++ src/components/Form/Resultimc/index.js
@@ -2,11 +2,11 @@ import React from "react"
 import { View, Text } from "react-native"
 import { clickProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
-export default function ResultImc(){
+export default function ResultImc(props){
     return(
         <View>
-            <Text>{props.ResultImc}</Text>
             <Text>{props.messageResultImc}</Text>
+            <Text>{props.resultImc}</Text>
         </View>
         );
 }
